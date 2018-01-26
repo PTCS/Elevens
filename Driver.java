452fd2af0dc0474e67c23ea87683d3a80d12ad94
Matt Fu <mrfu@parktudor.org>
2018-01-26 09:18:23 -0500

    Changed Driver to make sure it worked

--- Driver.java
+++ Driver.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;	
 
 public class Driver {
     public static void main(String[] args) {
@@ -12,7 +13,39 @@ public class Driver {
 
             if (!game.isOver()) {
                 String[] moves = in.nextLine().split(" ");
-                game.processMoves(moves);
+		if(moves.length!=2){
+		    boolean contains = Arrays.asList(moves).contains("J");
+		    contains = contains && Arrays.asList(moves).contains("Q");
+		    contains = contains && Arrays.asList(moves).contains("K");
+		    if(contains){
+			game.processMoves(moves);	
+		    }
+		}
+		else if(moves.length == 2){
+		    if(moves[0].equals("A"))
+			moves[0] = "1";
+		    if(moves[1].equals("A"))
+			moves[1]="1";
+		    if(moves[0].equals("T"))
+			moves[0]="10";
+		    if(moves[1].equals("T"))
+			moves[1]="10";
+		    try{
+		    	int temp = Integer.parseInt(moves[0]);
+		    	int temp1 = Integer.parseInt(moves[1]);
+		    	if(temp+temp1==11){
+			    game.processMoves(moves);
+		    	}
+		    }catch(Exception e){
+			System.out.println("Your input was not accepted" +"\n" + "Please enter another");
+			
+		    }	
+		}
+		else{
+		    System.out.println("Your input was not accepted"+"\n"+"Please enter another");
+		}
+		System.out.println("");
+                
             }
         } while(!game.isOver());
 
