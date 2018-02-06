bb72641a4b20a692399565dc851734ec898105f9
clombardo99 <clombardo@parktudor.org>
2018-02-06 17:51:00 +0000

    really nothing, some aesthetic changes

--- Board.java
+++ Board.java
@@ -70,7 +70,7 @@ public class Board {
      */
     public String toString() {
 
-        String s = "";
+        String s = "BOARD:" + "\n";
 
         for(int r = 0; r < board.length; r++) {
             for(int c = 0; c < board[0].length; c++) {
