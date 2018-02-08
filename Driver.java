9c49d17e0982317034e605a9efacffb745b1023e
clombardo99 <clombardo@parktudor.org>
2018-02-08 20:05:25 +0000

    intro message

--- Driver.java
+++ Driver.java
@@ -6,6 +6,13 @@ public class Driver {
         Scanner in = new Scanner(System.in);
 
         Elevens game = new Elevens();
+	
+	System.out.println("Welcome to Elevens!");
+	System.out.println("Here is an explanation of the rules:");
+	System.out.println("Look at the nine cards on the board and find two that add up to Eleven.");
+	System.out.println("If no two cards exist, you can also choose a Jack, Queen, and King.");
+	System.out.println("if neither of these conditions exist, you lose!");
+	System.out.println("But if you burn through the entire deck, you win!");
 
         do {
             System.out.println(game.getBoard());
